2c8a58bdd2f230fa5c66cda2a3c3684a172e80fb
Ruro86 <olidepoorter@hotmail.com>
2022-09-21 14:16:13 +0200

    commit

---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+az login
+
+az account list
+
+az ad sp create-for-rbac --name spgitaccount --role contributor --scopes /subscriptions/19fb3629-ec5b-470b-9630-1cfa5462d705 --sdk-auth
+
+
+"clientId": "a4f043f3-c27a-4eb9-9cf3-a6bfa1b3e8d0",
+"clientSecret": "I~V8Q~PIQwRJaW6RjsadkwqeMrTWCWImeUvLHlQq_ZaOM",
+"subscriptionId": "19fb3629-ec5b-470b-9630-1cfa5462d705",
+"tenantId": "6546ad83-dbad-417b-a9e7-458037005b40",
